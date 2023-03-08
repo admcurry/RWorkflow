eae4fd6e56279c3cbe66137d2184dce431cfed3f
atcurry <adambcurry@outlook.com>
2023-03-08 22:45:57 +0000

    Completed Graph

--- Datasets/Diamonds/Diamonds.R
+++ Datasets/Diamonds/Diamonds.R
@@ -3,4 +3,8 @@ library(tidyverse)
 load(file = 'diamonds.RData')
 
 ggplot(diamonds, aes(carat, price, color = clarity)) +
-  geom_point(alpha=0.5, shape = 16)
+  geom_point(alpha=0.5, shape = 16, size=1.7) +
+  labs(title = 'Price Against Carat of Diamonds, Coloured by Clarity',
+       x = 'Carat',
+       y= 'Price in $',
+       color='Clarity')
